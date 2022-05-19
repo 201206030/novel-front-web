9ae00b09020c0657c1072894ad57f1ec26854982
xiongxiaoyang <773861846@qq.com>
2022-05-19 21:35:34 +0800

    fix: 通过请求拦截来添加 Authorization Header

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -8,9 +8,8 @@
             ><a href="/">网站首页</a><i class="line">|</i
             ><a href="/about/default.html">关于我们</a><i class="line">|</i
             ><a href="/about/contact.html">联系我们</a><i class="line">|</i
-            ><router-link :to="{ name: 'feadBack' }" class="line" >反馈留言</router-link><i class="line">|</i
-            ><a href="/author/index.html">作家专区</a><i class="line">|</i
-            ><a href="/mobile/fiction_house.apk">客户端</a>
+            ><a href="javascript:void(0)" @click="goFeedBack">反馈留言</a><i class="line">|</i><a href="/author/index.html">作家专区</a
+            ><i class="line">|</i><a href="/mobile/fiction_house.apk">客户端</a>
           </li>
           <li>
             Copyright (C) xiongxyang.gitee.io All rights
@@ -23,8 +22,23 @@
 </template>
 
 <script>
+import { useRouter } from "vue-router";
+import { getToken } from "@/utils/auth";
 export default {
-  name: "Footer"
+  name: "Footer",
+  setup() {
+    const router = useRouter();
+    const goFeedBack = () => {
+      if(!getToken()){
+        router.push({name: 'login'});
+      }else{
+      router.push({name: 'feadBack'});
+      }
+    };
+    return {
+      goFeedBack
+    }
+  },
   
 };
 </script>
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import router from '@/router'
 import { ElMessage } from 'element-plus'
-import {getToken,removeToken, setToken} from '@/utils/auth'
+import {getToken,removeToken,removeNickName, setToken} from '@/utils/auth'
 
 
 
@@ -9,8 +9,15 @@ axios.defaults.baseURL = process.env.VUE_APP_BASE_API_URL
 axios.defaults.timeout = 10000
 axios.defaults.withCredentials = true
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
-axios.defaults.headers['Authorization'] = getToken() || ''
 axios.defaults.headers.post['Content-Type'] = 'application/json'
+
+axios.interceptors.request.use(config => {
+  config.headers['Authorization'] = getToken()
+  return config
+}, error => {
+    console.log(error)
+    Promise.reject(error)
+})
 
 axios.interceptors.response.use(res => {
   if (typeof res.data !== 'object') {
@@ -25,6 +32,7 @@ axios.interceptors.response.use(res => {
     if (res.data.code == 'A0230') {
       // 移除 token 
       removeToken();
+      removeNickName();
       router.push({ path: '/login' })
     }
 
