6d60a133a929a76fba563895b16b6365c9d484ca
xiongxiaoyang <1179705413@qq.com>
2023-04-25 13:33:46 +0800

    去掉无效链接

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,16 +3,9 @@
     <div class="box_center cf">
       <div class="copyright">
         <ul>
-          <li class="menu">
-            <a href="/?to=mobile">手机站</a><i class="line">|</i
-            ><a href="/">网站首页</a><i class="line">|</i
-            ><a href="/about/default.html">关于我们</a><i class="line">|</i
-            ><a href="/about/contact.html">联系我们</a><i class="line">|</i
-            ><a href="javascript:void(0)" @click="goFeedBack">反馈留言</a><i class="line">|</i><a href="/author/index.html">作家专区</a
-            ><i class="line">|</i><a href="/mobile/fiction_house.apk">客户端</a>
-          </li>
+          
           <li>
-            Copyright (C) xiongxyang.gitee.io All rights
+            Copyright (C) xxyopen.com All rights
             reserved&nbsp;&nbsp;小说精品屋版权所有
           </li>
         </ul>
